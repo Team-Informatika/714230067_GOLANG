ee060a1dafea6b47cf2f3345d3cacc779ff5715b
IndahPanjaitan <email@example.com>
2025-05-27 00:20:35 +0700

    push

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Mahasiswa struct {
+	Nama    string `json:"nama"`
+	NPM     string `json:"npm"`
+	Umur    int    `json:"umur"`
+	Jurusan string `json:"jurusan"`
+}
+
+func GetMahasiswa(c *fiber.Ctx) error {
+	data := Mahasiswa{
+		Nama:    "Galuh Sanjaya Putra",
+		NPM:     "714230067",
+		Umur:    20,
+		Jurusan: "Teknik Informatika",
+	}
+	return c.JSON(data)
+}
+func main() {
+	app := fiber.New()
+
+	app.Get("/mahasiswa", GetMahasiswa)
+
+	app.Listen(":3000")
+}
